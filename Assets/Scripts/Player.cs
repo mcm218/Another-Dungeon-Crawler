57f7045cb775c06bc607f5d77472785338880ec1
Michael Muniz <mcmuniz218@gmail.com>
2024-02-29 08:40:13 -0500

    tweak movement

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -35,13 +35,17 @@ public class Player : MonoBehaviour {
         if (movementInput != Vector2.zero) {
             Vector2 currentMovementDirection = rb.velocity.normalized;
             
+            float dotProduct = Vector2.Dot(currentMovementDirection, movementInput);
+            float lerpValue  = (dotProduct + 1) / 2;
+            float forceMagnitude = Mathf.Lerp(acceleration, deceleration, lerpValue);
+            Vector2 force = movementInput.normalized * forceMagnitude;
+            
             // If changing direction, should use deceleration value
-            float verticalForce   = Math.Abs(Mathf.Sign(movementInput.x) - Mathf.Sign(currentMovementDirection.x)) < 0.1f ? movementInput.y * acceleration : movementInput.y * deceleration;
-            float horizontalForce = Math.Abs(Mathf.Sign(movementInput.y) - Mathf.Sign(currentMovementDirection.y)) < 0.1f ? movementInput.x * acceleration : movementInput.x * deceleration;
+            // float verticalForce   = Math.Abs(Mathf.Sign(movementInput.x) - Mathf.Sign(currentMovementDirection.x)) < 0.1f ? movementInput.y * acceleration : movementInput.y * deceleration;
+            // float horizontalForce = Math.Abs(Mathf.Sign(movementInput.y) - Mathf.Sign(currentMovementDirection.y)) < 0.1f ? movementInput.x * acceleration : movementInput.x * deceleration;
             // if (Mathf.Sign(movementInput.x) != Mathf.Sign(currentMovementDirection.x)) Debug.Log("Changing horizontal direction");
             // if (Mathf.Sign(movementInput.y) != Mathf.Sign(currentMovementDirection.y)) Debug.Log("Changing vertical direction");
             
-            Vector2 force = new Vector2(horizontalForce, verticalForce);
             
             rb.AddForce(force, ForceMode2D.Force);
             
