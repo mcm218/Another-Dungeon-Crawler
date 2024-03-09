3958394b3e2f4a9118e3209c83fc2570a167e88e
Michael Muniz <mcmuniz218@gmail.com>
2024-03-09 09:58:59 -0500

    wiggles r us

--- Assets/Scripts/Enemies/EnemyController.cs
+++ Assets/Scripts/Enemies/EnemyController.cs
@@ -1,9 +1,10 @@
 using Interfaces;
 using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace Enemies {
-    [RequireComponent(typeof(CircleCollider2D), typeof(CircleCollider2D), typeof(BasicEnemy))]
+    [RequireComponent(typeof(CircleCollider2D), typeof(CircleCollider2D))]
     public class EnemyController : MonoBehaviour {
         [SerializeField]
         private GameObject target;
@@ -11,13 +12,16 @@ namespace Enemies {
         private IHealth lockedTarget;
         
         private EnemyModel       model;
-        private BasicEnemy       enemy;
+        public BasicEnemy       enemy;
         
         private CircleCollider2D AttackCollider => enemy.AttackCollider;
         private Collider2D Body => enemy.Body;
         private IWeapon Weapon => enemy.Weapon;
         
         private float timeSinceLastAttack = 0f;
+        
+        public List<Transform> children    = new List<Transform>();
+        public Vector3         childOffset = Vector3.zero;
         
         
         private void Awake() {
@@ -42,14 +46,18 @@ namespace Enemies {
             enemy.Health = model.health;
         }
         
+        float wiggleDistance = 10f;
+        float wiggleSpeed    = 5;
+        
 
         private void Update() {
             if (target == null) return;
-            var direction = target.transform.position - transform.position;
-            var angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
-            transform.rotation = Quaternion.Euler(0, 0, angle);
+            // var direction = target.transform.position - transform.position;
+            // var angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+            // transform.rotation = Quaternion.Euler(0, 0, angle);
             
             transform.position = Vector3.MoveTowards(transform.position, target.transform.position, model.speed * Time.deltaTime);
+            children.ForEach(child => child.position = Vector3.MoveTowards(child.position, target.transform.position + childOffset, model.speed * Time.deltaTime));
             
             if (lockedTarget != null) {
                 timeSinceLastAttack += Time.deltaTime;
@@ -58,7 +66,11 @@ namespace Enemies {
                     timeSinceLastAttack = 0f;
                 }
             }
+            float yPosition = Mathf.Sin(Time.time * wiggleSpeed) * wiggleDistance;
+            transform.localRotation = Quaternion.Euler(0, 0, yPosition);
         }
+        
+ 
         
         private readonly string[] TAGS_TO_ATTACK = {"Player"};
         
