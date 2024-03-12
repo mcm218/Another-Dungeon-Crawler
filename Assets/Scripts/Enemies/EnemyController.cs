66b5f8d81ba863c1ec4a8df9e9424a78baa0b6ef
Michael Muniz <mcmuniz218@gmail.com>
2024-03-12 16:25:12 -0400

    temp fix

--- Assets/Scripts/Enemies/EnemyController.cs
+++ Assets/Scripts/Enemies/EnemyController.cs
@@ -42,17 +42,16 @@ namespace Enemies {
         }
 
         private void Start() {
-            
             AttackCollider.isTrigger = true;
             enemy.Health = model.health;
-            if (Sword != null) {
-                AttackCollider.radius    = Sword.GetRange();
-                Sword.Equip(enemy);
-            }
-            if (Gun != null) {
-                AttackCollider.radius = Gun.GetRange();
-                Gun.Equip(enemy);
-            }
+            // if (Sword != null) {
+            //     AttackCollider.radius    = Sword.GetRange();
+            //     Sword.Equip(enemy);
+            // }
+            // if (Gun != null) {
+            //     AttackCollider.radius = Gun.GetRange();
+            //     Gun.Equip(enemy);
+            // }
             randomOffset = Random.Range(0, 100f);
         }
         
@@ -69,16 +68,16 @@ namespace Enemies {
             enemy.GameObject.transform.position = Vector3.MoveTowards(enemy.GameObject.transform.position, target.transform.position, model.speed * Time.deltaTime);
             children.ForEach(child => child.position = enemy.GameObject.transform.position + childOffset);
             
-            if (lockedTarget != null && Gun != null && Gun.CanAttack()) {
-                // Gun.PerformAttack();
-            }
-            
-            if (lockedTarget != null && Sword != null && Sword.CanAttack()) {
-                // Sword.PerformAttack();
-            }
-            float yPosition = Mathf.Sin((Time.time + randomOffset) * wiggleSpeed) * wiggleDistance;
-            enemy.GameObject.transform.localRotation = Quaternion.Euler(0, 0, yPosition);
-            children.ForEach(child => child.localRotation = Quaternion.Euler(0, 0, -yPosition));
+            // if (lockedTarget != null && Gun != null && Gun.CanAttack()) {
+            //     // Gun.PerformAttack();
+            // }
+            //
+            // if (lockedTarget != null && Sword != null && Sword.CanAttack()) {
+            //     // Sword.PerformAttack();
+            // }
+            // float yPosition = Mathf.Sin((Time.time + randomOffset) * wiggleSpeed) * wiggleDistance;
+            // enemy.GameObject.transform.localRotation = Quaternion.Euler(0, 0, yPosition);
+            // children.ForEach(child => child.localRotation = Quaternion.Euler(0, 0, -yPosition));
         }
         
  
